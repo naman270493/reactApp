40abaed6ce3ad0f7b3ac4fac1d5fbed968909681
naman270493 <37108329+naman270493@users.noreply.github.com>
2018-07-25 20:24:16 +0530

    Create prodData.js

---
+++ data/prodData.js
@@ -0,0 +1,59 @@
+var data = [{
+  name: 'Jane Austen',
+  imageURL: 'https://raw.githubusercontent.com/khyamay/react-quiz/master/images/authors/jane-austen.jpg',
+  books: [
+    'Pride and Prejudice',
+    'Sense and Sensibility',
+    'Emma'
+  ]
+}, {
+  name: 'Joseph Conrad',
+  imageURL: 'https://raw.githubusercontent.com/khyamay/react-quiz/master/images/authors/joseph-conrad.png',
+  books: [
+    'Heart of Darkness'
+  ]
+}, {
+  name: 'Charles Dickens',
+  imageURL: 'https://raw.githubusercontent.com/khyamay/react-quiz/master/images/authors/charles-dickens.jpg',
+  books: [
+    'A Tale of Two Cities',
+    'A Christmas Carol',
+    'David Copperfield',
+    'Bleak House'
+  ]
+}, {
+  name: 'Sigmunnd Freud',
+  imageURL: 'https://raw.githubusercontent.com/khyamay/react-quiz/master/images/authors/sigmund-freud.jpg',
+  books: [
+    'Jokes and Their Relation to the Unconscious',
+    'Civilization and Its Discontents',
+    'The Interpretation of Dreams'
+  ]
+}, {
+  name: 'Friedrich Nietzsche',
+  imageURL: 'https://raw.githubusercontent.com/khyamay/react-quiz/master/images/authors/friedrich-nietzsche.jpg',
+  books: [
+    'Thus Spake Zarathustra',
+    'Ecce Homo',
+    'Beyond Good and Evil',
+    'Twilight of the Idols'
+  ]
+}, {
+  name: 'William Shakespeare',
+  imageURL: 'https://raw.githubusercontent.com/khyamay/react-quiz/master/images/authors/william-shakespeare.jpg',
+  books: [
+    'King Lear',
+    'A Midsummer Night\'s Dream',
+    'Hamlet',
+    'Richard III',
+    'The Comedy of Errors'
+  ]
+}, {
+  name: 'Mark Twain',
+  imageURL: 'https://raw.githubusercontent.com/khyamay/react-quiz/master/images/authors/mark-twain.jpg',
+  books: [
+    'Huckleberry Finn',
+    'Tom Sawyer',
+    'A Connecticut Yankee at King Arthur\'s Court'
+  ]
+}];
