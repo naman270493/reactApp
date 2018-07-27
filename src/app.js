f3342026b0099fb0909720da923b07690fea7956
naman270493 <37108329+naman270493@users.noreply.github.com>
2018-07-27 22:12:18 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,21 +1,24 @@
 import React, {Component} from "react";
+import Main from "./main.js";
 
-class App extends Component{
 
-function Header(){
-	return(<div><h1>Author Quiz</h1><p>Select the books of author below</p></div>);
-}
 function Footer(){
 	return(<div><p>Contact US</p></div>);
 }
+function Header(){
+	return (<div><h1>Author Quiz</h1><p>Select the books of author below</p></div>);
+}
 
+
+class App extends Component{
 
 render(){
+
 	return (
 			<div>
-			<Header />
-			<main{...mainData}/>
-			<Continue/>
+			<Header/>
+			//<Main {...state}/>
+			<Main/>
 			<Footer/>
 			</div>
 	);
