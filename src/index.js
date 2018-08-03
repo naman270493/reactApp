657403f3d097c6b94ecabffba8de907fae1fa1cf
naman270493 <37108329+naman270493@users.noreply.github.com>
2018-08-03 23:53:09 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,20 +1,22 @@
-import React from "react";
+import React, {Component} from "react";
 import ReactDOM from "react-dom";
-import data from "./data.js";
-import App from "./app.js";
+import data from "./prodData.js";
+import "./bootstrap.min.css";
+import "./app.css";
+
 //import registerServiceWorker from './registerServiceWorker';
 import {shuffle, sample} from 'underscore';
 
 const authorData= data;
 
-function getData(){
-	const bookData = authorData.reduce((p,c,i)=>p.books.concat(c.books));
+function getData(authorData){
+	const bookData = authorData.reduce((p,c,i)=>p.concat(c.books),[]);
 	const fourRandomBooks = shuffle(bookData).slice(0,4);
 	const answer = sample(fourRandomBooks);
 	
 	return{
 			books: fourRandomBooks,
-			author: authors.find((author)=>authors.books.some((title)=>(title==answer)))
+			author: authorData.find((author)=>author.books.some((title)=>(title==answer)))
 	};
 	 }		
 /*			var bookdata =[];
@@ -35,16 +37,55 @@ function getData(){
 	 }
 */
 
+function Footer(){
+	return(<div class="jumbotron center"><p><strong>Contact US</strong></p></div>);
+}
+function Header(mainData){
+	return (<div class="jumbotron center"><h1>Author Quiz</h1><p>Select the books of author below</p></div>);
+}
 const state = {
-			//mainData : getData()
-		 mainData : 	{	
+			mainData : getData(authorData)
+		 //mainData : 	{	
 							//author :authorData[0].name,
 							//books : authorData[0].books
-							author:"naman",
-							books:"21 days in future"
-			}
+					
+			//}
 		
 	}
-ReactDOM.render(<App/>, document.getElementById('root'));
-//React.render(<App {...state}/>, document.getElementById('root'));
+	
+function Main({mainData}){
+	
+	
 
+	//var data = props.data;
+		return(
+			
+			<div className="container-fluid">
+			<div className="authnamecss"><strong>{mainData.author.name}</strong></div>
+			<div className= "row">
+				<div className= "col-md-4 offset-1" >
+					<img src={mainData.author.imageURL} className="authorimage"/>
+			</div>
+				<div className= "col-md-6 offset-1">
+					{mainData.books.map(function(title){ return (<p>{title}</p>);})}
+			</div>
+			</div>
+			</div>
+		);
+	
+	
+	
+}
+
+function App (mainData){
+
+	return (
+			<div>
+			<Header />
+			<Main {...mainData}/>
+			<Footer />
+			</div>
+	);
+}
+//ReactDOM.render(<App/>, document.getElementById('root'));
+ReactDOM.render(<App  {...state}/>, document.getElementById('root'));
