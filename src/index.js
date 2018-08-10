91697409c4eeb0c08e2ffe086c503f8e62621276
naman270493 <37108329+naman270493@users.noreply.github.com>
2018-08-10 23:48:20 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -38,54 +38,67 @@ function getData(authorData){
 */
 
 function Footer(){
-	return(<div class="jumbotron center"><p><strong>Contact US</strong></p></div>);
+	return(<div className="jumbotron center"><p><strong>Contact US</strong></p></div>);
 }
 function Header(mainData){
-	return (<div class="jumbotron center"><h1>Author Quiz</h1><p>Select the books of author below</p></div>);
+	return (<div className="jumbotron center"><h1>Author Quiz</h1><p>Select the books of author below</p></div>);
 }
 const state = {
-			mainData : getData(authorData)
+			mainData : getData(authorData),
 		 //mainData : 	{	
 							//author :authorData[0].name,
 							//books : authorData[0].books
 					
 			//}
-		
+			highlight : ''
 	}
-	
-function Main({mainData}){
-	
-	
 
-	//var data = props.data;
+	function Book({title, onClick}){
+		return (<div className = "answer" onClick={()=>{onClick(title);}}>
+				<h3>{title}</h3>
+				</div>
+		);
+
+}
+	
+function Main({mainData, highlight}){
+
+		
 		return(
 			
 			<div className="container-fluid">
 			<div className="authnamecss"><strong>{mainData.author.name}</strong></div>
-			<div className= "row">
+			<div className= "row" style={{backgroundColor: state.highlight}}>
 				<div className= "col-md-4 offset-1" >
 					<img src={mainData.author.imageURL} className="authorimage"/>
 			</div>
 				<div className= "col-md-6 offset-1">
-					{mainData.books.map(function(title){ return (<p>{title}</p>);})}
+					{mainData.books.map(function(title){ return (<Book title={title} key={title} onClick={onAnswerSelected}/>);})}
 			</div>
 			</div>
 			</div>
 		);
-	
-	
-	
 }
 
-function App (mainData){
+function App (mainData, highlight, onAnswerSelected){
 
 	return (
 			<div>
 			<Header />
-			<Main {...mainData}/>
+			<Main {...mainData} highlight={highlight} onAnswerSelected= {onAnswerSelected}/>
 			<Footer />
 			</div>
 	);
 }
+
+function onAnswerSelected(answer){
+				const isCorrect = state.mainData.author.books.some((book)=> book === answer);
+				state.highlight= isCorrect ? 'green' : 'red';
+				render();
+		}
+		
 //ReactDOM.render(<App/>, document.getElementById('root'));
-ReactDOM.render(<App  {...state}/>, document.getElementById('root'));
+function render(){
+ReactDOM.render(<App  {...state} onAnswerSelected= {onAnswerSelected} />, document.getElementById('root'));
+}
+render();
